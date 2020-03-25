4e6f4a6a38e669390cca5e3c7ec91d08fdc0e0e1
Ganesh <36333917+oarcia@users.noreply.github.com>
2020-03-25 00:03:37 -0600

    Actualización con desafió

--- main.tf
+++ main.tf
@@ -22,6 +22,29 @@ resource "aws_subnet" "subnet_course" {
     Name = "Subnet_course_1"
   }
 }
+#subnet de desafio
+resource "aws_subnet" "subnet_course2" {
+  vpc_id = "${aws_vpc.example.id}"
+  cidr_block = "10.0.20.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "us-west-2b"
+
+  tags = {
+    Name = "Subnet_course_2-2b"
+  }
+}
+#subnet de desafio
+resource "aws_subnet" "subnet_course3" {
+  vpc_id = "${aws_vpc.example.id}"
+  cidr_block = "10.0.25.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "us-west-2c"
+
+  tags = {
+    Name = "Subnet_course_3-2c"
+  }
+}
+
 
 resource "aws_internet_gateway" "gw_course" {
   vpc_id = "${aws_vpc.example.id}"
@@ -45,4 +68,13 @@ resource "aws_route_table_association" "rtbl_course_subnet_1" {
   subnet_id = "${aws_subnet.subnet_course.id}"
   route_table_id = "${aws_route_table.rtb_course.id}"
 }
-
+#asociacion para el desafio
+resource "aws_route_table_association" "rtbl_course_subnet_2" {
+  subnet_id = "${aws_subnet.subnet_course2.id}"
+  route_table_id = "${aws_route_table.rtb_course.id}"
+}
+#asociacion para el desafio
+resource "aws_route_table_association" "rtbl_course_subnet_3" {
+  subnet_id = "${aws_subnet.subnet_course3.id}"
+  route_table_id = "${aws_route_table.rtb_course.id}"
+}
