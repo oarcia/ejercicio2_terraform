c2ef35d117ca8d41e991b273903229918d3c0a06
Ganesh <36333917+oarcia@users.noreply.github.com>
2020-03-24 23:30:40 -0600

    Cambios visibles para la creacion de gateway

--- main.tf
+++ main.tf
@@ -23,3 +23,26 @@ resource "aws_subnet" "subnet_course" {
   }
 }
 
+resource "aws_internet_gateway" "gw_course" {
+  vpc_id = "${aws_vpc.example.id}"
+  tags = {
+    Name = "gtw_course"
+  }
+}
+
+resource "aws_route_table" "rtb_course" {
+  vpc_id = "${aws_vpc.example.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw_course.id}"
+  }  
+    tags = {
+      Name = "rtbl_course"
+    }
+}
+
+resource "aws_route_table_association" "rtbl_course_subnet_1" {
+  subnet_id = "${aws_subnet.subnet_course.id}"
+  route_table_id = "${aws_route_table.rtb_course.id}"
+}
+
